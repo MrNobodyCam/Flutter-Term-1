210b39f34147c60466838d4af0190c1d5077f798
Netspecter <158697772+MrNobodyCam@users.noreply.github.com>
2024-11-06 00:51:25 +0700

    ready w4

--- flutter/lib/W4 - S3 Stateful Widget - PRACTICE/EX-2/ex_2_step_1_start.dart
+++ flutter/lib/W4 - S3 Stateful Widget - PRACTICE/EX-2/ex_2_step_1_start.dart
@@ -2,17 +2,44 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: const Text("Favorite cards"),
-        ),
-        body: Column(
-          children: [
-            Container(
-                // YOUR CODE
-                )
-          ],
-        ),
-      ),
+          backgroundColor: Colors.white,
+          body: Column(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(width: .5, color: Colors.grey),
+                  ),
+                ),
+                padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Expanded(
+                        flex: 7,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Title",
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text("Description"),
+                            SizedBox(height: 10.0),
+                          ],
+                        )),
+                    IconButton(
+                      onPressed: () => {},
+                      icon: const Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )),
     ));
--- flutter/lib/W4 - S3 Stateful Widget - PRACTICE/EX-3/ex_3_start.dart
+++ flutter/lib/W4 - S3 Stateful Widget - PRACTICE/EX-3/ex_3_start.dart
@@ -8,29 +8,65 @@ List<String> images = [
   "assets/w4-s2/man.jpg",
 ];
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false, // Why this line ? Can you explain it ?
-      home: Scaffold(
-        backgroundColor: Colors.green[50],
-        appBar: AppBar(
-          backgroundColor: Colors.green[400],
-          title: const Text('Image viewer'),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.navigate_before),
-              tooltip: 'Go to the previous image',
-              onPressed: () => {},
+class ChangeImage extends StatefulWidget {
+  const ChangeImage({super.key});
+
+  @override
+  State<ChangeImage> createState() => _ChangeImageState();
+}
+
+class _ChangeImageState extends State<ChangeImage> {
+  int index = 0;
+  void nextImage() {
+    setState(() {
+      index++;
+      if (index > 4) {
+        index = 0;
+      }
+    });
+  }
+
+  void previousImage() {
+    setState(() {
+      index--;
+      if (index < 0) {
+        index = images.length - 1;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[50],
+      appBar: AppBar(
+        backgroundColor: Colors.green[400],
+        title: const Text('Image viewer'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.navigate_before),
+            tooltip: 'Go to the previous image',
+            onPressed: previousImage,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
+            child: IconButton(
+              icon: const Icon(Icons.navigate_next),
+              tooltip: 'Go to the next image',
+              onPressed: nextImage,
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
-              child: IconButton(
-                icon: const Icon(Icons.navigate_next),
-                tooltip: 'Go to the next image',
-                onPressed: () => {},
-              ),
-            ),
-          ],
-        ),
-        body: Image.asset(images[0]),
+          ),
+        ],
       ),
+      body: Image.asset(images[index]),
+    );
+  }
+}
+
+void main() => runApp(const MaterialApp(
+      //We use to hide the red line debug in top right of screen
+      debugShowCheckedModeBanner: false, // Why this line ? Can you explain it ?
+      home: ChangeImage(),
     ));
+// Type of state that i need to keep is local state
+// Widget that i need to warp into your stateful widget is Scarful because it can show all data that i need in body and appBar
