f8cd12ad89e1aa590b3e78a2b632e183ddb39328
Netspecter <kaovichet11@gmail.com>
2024-11-17 22:49:52 +0700

    bonus ready

---
+++ flutter/lib/W5 - S1 - PRACTICE- Lists, Pass Functions/Bonus/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:lutter/W5%20-%20S1%20-%20PRACTICE-%20Lists,%20Pass%20Functions/EXERCISE-3/screen/temperature.dart';
+import 'package:lutter/W5%20-%20S1%20-%20PRACTICE-%20Lists,%20Pass%20Functions/EXERCISE-3/screen/welcome.dart';
+
+class TemperatureApp extends StatefulWidget {
+  const TemperatureApp({super.key});
+
+  @override
+  State<TemperatureApp> createState() => _TemperatureAppState();
+}
+
+class _TemperatureAppState extends State<TemperatureApp> {
+  bool isClicked = false;
+  void checkClick() {
+    setState(() {
+      isClicked = !isClicked;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xff16C062), Color(0xff00BCDC)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: isClicked ? Temperature() : Welcome(checkClick: checkClick),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(const TemperatureApp());
+}
---
+++ flutter/lib/W5 - S1 - PRACTICE- Lists, Pass Functions/Bonus/screen/temperature.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class Temperature extends StatefulWidget {
+  Temperature({super.key});
+
+  @override
+  State<Temperature> createState() => _TemperatureState();
+}
+
+class _TemperatureState extends State<Temperature> {
+  final TextEditingController _controller = TextEditingController();
+
+  double inputValue = 0.0;
+  double fahrenheit = 0.0;
+
+  final BoxDecoration textDecoration = BoxDecoration(
+    color: Colors.white,
+    borderRadius: BorderRadius.circular(12),
+  );
+
+  final InputDecoration inputDecoration = InputDecoration(
+      enabledBorder: OutlineInputBorder(
+        borderSide: const BorderSide(color: Colors.white, width: 1.0),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      hintText: 'Enter a temperature',
+      hintStyle: const TextStyle(color: Colors.white));
+
+  void printInput() {
+    setState(() {
+      inputValue = double.tryParse(_controller.text) ?? 0.0;
+      fahrenheit = inputValue * 9 / 5 + 32;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(40.0),
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Icon(
+            Icons.thermostat_outlined,
+            size: 120,
+            color: Colors.white,
+          ),
+          const Center(
+            child: Text(
+              "Converter",
+              style: TextStyle(color: Colors.white, fontSize: 45),
+            ),
+          ),
+          const SizedBox(height: 50),
+          const Text("Temperature in Degrees:"),
+          const SizedBox(height: 10),
+          TextField(
+              controller: _controller,
+              decoration: inputDecoration,
+              style: const TextStyle(color: Colors.white)),
+          const SizedBox(height: 30),
+          const Text("Temperature in Fahrenheit:"),
+          const SizedBox(height: 10),
+          Container(
+            padding: const EdgeInsets.all(10),
+            decoration: textDecoration,
+            child: Text(fahrenheit.toString()),
+          ),
+          ElevatedButton(
+            onPressed: printInput, // Call function when button is pressed
+            child: const Text('Print Temperature'),
+          ),
+        ],
+      )),
+    );
+  }
+}
+
+class _controller {}
